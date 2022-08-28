33829566a7185d0acf14546ecc2ccf62da78424f
ZachCodedThat <Zachemailsback@gmail.com>
2022-08-28 11:51:58 -0400

    made a "better" URL validation.

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,26 @@ import { z } from "zod";
 import { useZorm } from "react-zorm";
 import zachInfo from "./zachInfo.json";
 
+/* I decided to use Zods regex to parse the incoming url and check if it is valid. I dont tend to use regex unless it is a very specific and small use case and this fit the bill.
+
+    I am convinced that there is a way to pass all acceptable values into a single endsWith() call but the answer elludes me, as far as I can tell 
+    endsWith() accepts a single string and an optional error message to display if the string does not match. 
+    
+    If I figfure out a way to pass multiple values (array map?, tuple?? etc) I will update this to reflect it but it was eating at me this weekend and I had to enhance this a bit. 
+  
+  
+  **/
+
+const VALID_IMAGE_URL = z
+  .string()
+  .regex(
+    /^https?:\/\/.*\.(?:png|jpg|jpeg|gif)$/,
+    "Must be a valid image url that ends in .png, .jpg, .jpeg, or .gif"
+  );
+
 const FormSchema = z.object({
-  name: z.string(),
-  avatarImageUrl: z.string().url("Must be a valid URL that points to an image"),
+  name: z.string().min(1, "Name must be at least 1 character long"),
+  avatarImageUrl: VALID_IMAGE_URL,
 });
 
 function ErrorMessage(props: { message: string }) {
@@ -28,6 +45,7 @@ export default function App() {
       });
     },
   });
+
   const disabled = zForm.validation?.success === false;
 
   const handleClick = () => {
